594da5133eaf9a4a523de680d74b09e6d04ea37d
Nataly Kozyreva <eve30@yandex.ru>
2023-07-28 17:15:57 +0300

    working on js

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ const errorDiv = document.getElementById('error');
 console.log(errorDiv);
 const dataDiv = document.getElementById('data');
 console.log(dataDiv);
+
+let arr = [];
+function getData() {
+fetch ('https://swapi.nomoreparties.co/planets/1')
+.then(function(response){
+    return response.json();
+})
+.then (function(user){
+    console.log(user)
+    // getUsers(user);
+})
+
+}
+getData();
+
+button.addEventListener('click', getData);
+
+
+//не то 
+    // function getUsers(arr) {
+    //     dataDiv.innerHTML = '';
+    //     Object.keys(arr).forEach(item=>{
+    // createTemplate(item)
+    //     })
+    // }
+
+    // function createTemplate(item){
+    //     const{name, diameter,climate} = item;
+    //     const template =
+    //     `<h1>${name}</h1>
+    //     <p>CLIMATE:${climate}<p>
+    //     <p>DIAMETER:${diameter}<p>
+    //     `
+    //     const div = document.createElement('div');
+    //     div.innerHTML = template;
+    //     dataDiv.append(div);
+    // }
